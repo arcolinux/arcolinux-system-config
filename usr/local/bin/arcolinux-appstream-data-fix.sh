78e453c87197b3c01dadf1bf5cba1d3f7754af76
Erik Dubois <erik.dubois@gmail.com>
2020-10-20 15:39:23 +0200

    no pacman - no sudo

--- usr/local/bin/arcolinux-appstream-data-fix.sh
+++ usr/local/bin/arcolinux-appstream-data-fix.sh
@@ -13,7 +13,6 @@
 
 #echo "Fix for icons not showing up in pamac-aur"
 
-sudo pacman -S appstream --noconfirm --needed
 zcat /usr/share/app-info/xmls/community.xml.gz | sed 's|<em>||g;s|<\/em>||g;' | gzip > "new.xml.gz"
-sudo mv new.xml.gz /usr/share/app-info/xmls/community.xml.gz
-sudo appstreamcli refresh-cache --force
+mv new.xml.gz /usr/share/app-info/xmls/community.xml.gz
+appstreamcli refresh-cache --force
